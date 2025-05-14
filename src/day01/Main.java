2f1d4cc3a0171c8f4e8139795668220539b59b4a
Song-Da <songowod@naver.com>
2025-05-14 19:14:34 +0900

    init: first study

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // 주석 처리: ctrl + /, 기능: 메모가 가능
         System.out.println("Hello java"); // 콘솔에 찍음
-        System.out.println("world");
+        System.out.println("world"); // world
+
     }
 }
