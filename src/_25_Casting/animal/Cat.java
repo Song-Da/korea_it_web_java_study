ed58e80e587e9045e47d0edb93c8484d2b2ffdfa
Song-Da <songowod@naver.com>
2025-05-27 21:36:34 +0900

    Feat: Casting

---
+++ src/_25_Casting/animal/Cat.java
@@ -0,0 +1,12 @@
+package _25_Casting.animal;
+
+public class Cat extends Animal{
+    @Override
+    public void speak() {
+        System.out.println("야옹");
+    }
+
+    public void jump() {
+        System.out.println("자리에서 점프한다.");
+    }
+}
---
+++ src/_25_Casting/animal/Tiger.java
@@ -0,0 +1,13 @@
+package _25_Casting.animal;
+
+public class Tiger extends Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("으르렁");
+    }
+
+    public void run() {
+        System.out.println("달린다.");
+    }
+}
